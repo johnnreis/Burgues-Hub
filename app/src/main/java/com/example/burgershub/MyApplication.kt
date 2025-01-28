74c87407ce18a1d0691c6bc1fb5795d3ad25037e
John Reis <jhonatareisy7@gmail.com>
2025-01-27 22:43:36 -0300

    first commit

---
+++ app/src/main/java/com/example/burgershub/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.example.burgershub
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+}
---
+++ app/src/main/java/com/example/burgershub/utils/Extensions.kt
@@ -0,0 +1,25 @@
+package com.example.burgershub.utils
+
+import com.google.gson.Gson
+import retrofit2.HttpException
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.text.NumberFormat
+import java.util.Locale
+
+inline fun <reified T> HttpException.getErrorResponse(): T? {
+    return try {
+        Gson().fromJson(response()?.errorBody()?.string(), T::class.java)
+    } catch (e: Exception) {
+        null
+    }
+}
+
+fun Float.formattedValue(): String? {
+    val nf: NumberFormat = DecimalFormat(
+        "#,##0.00", DecimalFormatSymbols(
+            Locale("pt", "BR")
+        )
+    )
+    return nf.format(this)
+}

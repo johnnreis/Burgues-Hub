a31d6a73a82ad2c7ec13e24624340560d3e7f683
John Reis <jhonatareisy7@gmail.com>
2025-01-29 02:09:57 -0300

    feat: configure service and requesting to network

--- app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
+++ app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
@@ -1,17 +1,33 @@
 package com.example.burgershub.network
 
 import com.example.burgershub.BuildConfig
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class ServiceProvider @Inject constructor() {
+class ServiceProvider {
 
     private val client = OkHttpClient.Builder()
         .connectTimeout(30, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+        .addInterceptor(Interceptor { chain ->
+            chain.run {
+                proceed(
+                    request()
+                        .newBuilder()
+                        .addHeader(
+                            name = "x-rapidapi-key",
+                            value = BuildConfig.API_KEY
+                        )
+                        .build()
+                )
+            }
+        })
         .build()
 
     private val retrofit = Retrofit.Builder()
@@ -19,6 +35,7 @@ class ServiceProvider @Inject constructor() {
         .addConverterFactory(GsonConverterFactory.create())
         .client(client)
         .build()
+
 
     fun <API> createService(apiClass: Class<API>) : API = retrofit.create(apiClass)
 
